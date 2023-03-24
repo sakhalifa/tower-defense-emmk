c67aca0dc0c9a6ff92dabc96f81b0a5ca26b4b98
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-03-24 14:47:03 +0100

    more eslint config :D

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,15 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     rules: {
-	"no-unused-vars": "off",
-	"@typescript-eslint/no-inferrable-types" : "off",
-	"@typescript-eslint/no-unused-vars": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+    },
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
     },
     root: true,
 };
