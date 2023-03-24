aa0c85f8f649d6b9f7a0d41bc3f13b47b7bdca97
DESCOMPS Théo <tdescomps@bordeaux-inp.fr>
2023-03-24 18:01:28 +0100

    initPhases

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { World } from "./world";
 import { Phase } from "./phase";
+import { Actor } from "./actor";
+import * as Actor_i from "./actor";
 
 function initWorld(): World {
 
 }
 
 function initPhases(): Array<Phase> {
-
+	return [Actor_i.createPhase("move", (a : Actor, w : World) => a)];
 }
 
 function computeNewWorld(w: World, phases: Array<(string)>) {
@@ -19,7 +21,7 @@ function initActors() {
 
 function main() {
 	let world = initWorld();
-	let phases = "move";
+	let phases = initPhases();
 	let actors = initActors();
 	let finished = false;
 	while (!finished) {
