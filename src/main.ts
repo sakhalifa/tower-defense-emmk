bb32d7ef2962f108eb25b14b98c7c897ffbad4b8
DESCOMPS Théo <tdescomps@bordeaux-inp.fr>
2023-03-25 19:06:16 +0100

    merge + resolveProposals wip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Matrix, matrixToString } from "./geometry";
-import { World } from "./world";
+import { World, isPositionInWorld } from "./world";
 import { Phase } from "./phase";
 import { Actor } from "./actor";
 import { createPhase } from "./phase";
@@ -8,37 +8,42 @@ function initWorld(): World {
 	throw Error();
 }
 
-function initPhases(): Array<("move")> {
-	throw Error();
-
-}
-
 function initPhases(): Array<Phase> {
 	return [createPhase("move", (a: Actor, w: World) => a)];
 }
 
-function computeNewWorld(w: World, phases: Array<("move")>): World {
+function computeNewWorld(w: World, phases: Array<Phase>): World {
+	throw Error();
+}
+
+function initActors() : Array<Actor> {
 	throw Error()
 }
 
-function initActors() {
-	throw Error()
+function resolveProposals(world : World, actors : Array<Actor>, proposals : Array<Actor>) : [World,Array<Actor>] {
+	proposals.map((currentActor) => {
+		if (isPositionInWorld(world, currentActor.pos)) {
+			return currentActor;
+		} else {
+			return currentActor;
+		}
+	});
+	return [world, proposals];
 }
 
 function main() {
-	let world = initWorld();
-	let phases = initPhases();
-	let actors = initActors();
-	let finished = false;
+	let world : World = initWorld();
+	let phases : Array<Phase> = initPhases();
+	let actors : Array<Actor> = initActors();
+	let finished : boolean = false;
 	while (!finished) {
-		[world, actors] = phases.reduce(([aWorld, actors], aPhase) => {
-			funcName: string = aPhase.funcName;
-			proposals = actors.map((anActor) =>
-				anActor.funcName(anActor, aWorld));
-			[aNewWorld, newActors] = resolveProposals(aWorld, actors, proposals);
+		[world, actors] = phases.reduce(([aWorld, someActors], aPhase) => {
+			const funcName : string = aPhase.funcName;
+			let proposals = someActors.map((anActor) =>
+				anActor[funcName](anActor, aWorld));
+			let [aNewWorld, newActors] = resolveProposals(aWorld, someActors, proposals);
 			return [aNewWorld, newActors];
-		},
-			[world, actors])
+		}, [world, actors])
 	}
 }
 
