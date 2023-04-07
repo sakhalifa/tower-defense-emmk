86ca94991176eb289c18acc0ee85555655883420
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2023-04-07 16:46:38 +0200

    Fixed actor size

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import type { World } from "./world";
 import type { Phase } from "./phase";
 
 import { initWorld, initPhases, nextTurn } from "./game";
+import { Vector2D, createVector } from "./geometry";
 
 const canvas: HTMLCanvasElement = document.getElementById("myCanvas") as HTMLCanvasElement;
 
@@ -13,9 +14,14 @@ async function displayWorldToCanvas(world: World){
     // Update canvas
     const ctx = canvas.getContext("2d");
     ctx?.clearRect(0, 0, canvas.width, canvas.height);
-    world.actors.forEach((a) => ctx?.fillRect(a.pos.x * 5, a.pos.y * 5, 5, 5));
+
+    const actorSize: Vector2D = createVector(canvas.width / world.width, canvas.height / world.height);
+
+    world.actors.forEach((a) => 
+        ctx?.fillRect(a.pos.x * actorSize.x, a.pos.y * actorSize.y, actorSize.x, actorSize.y));
+
     // wait
-    await delay(2000);
+    await delay(1000);
 }
 async function main(){
     let world: World = initWorld();
