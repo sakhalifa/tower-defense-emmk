3d69e2aea53c5f8160f3847f752a59fa403907d8
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-04-14 16:24:29 +0200

    fix tests

--- tst/world.test.ts
+++ tst/world.test.ts
@@ -3,13 +3,13 @@ import type { World } from "../src/world";
 import { createWorld, worldToString, isPositionInWorld, worldStringVectorToIndex } from "../src/world";
 
 test("createWorld test", () => {
-    expect(createWorld(50, 20)).toEqual({width: 50, height: 20});
-    expect(createWorld(-5, 5)).toThrowError();
-    expect(createWorld(5, 5.3)).toThrowError();
+    expect(createWorld(50, 20, 0)).toEqual({width: 50, height: 20, turnsElapsed: 0});
+    expect(createWorld(-5, 5, 0)).toThrowError();
+    expect(createWorld(5, 5.3, 0)).toThrowError();
 });
 
 test("isPositionInWorld test", () => {
-    const world = createWorld(10, 10);
+    const world = createWorld(10, 10, 0);
     // (0, 0) is in world
     expect(isPositionInWorld(world, createVector(0, 0))).toBeTruthy();
     // Position at limit is excluded
@@ -29,25 +29,25 @@ test("isPositionInWorld test", () => {
     expect(isPositionInWorld(world, createVector(1.2, 9.99999))).toBeTruthy();
 
     // World consisting of a line
-    const lineWorld = createWorld(3, 1);
+    const lineWorld = createWorld(3, 1, 0);
 
     expect(isPositionInWorld(lineWorld, createVector(1, 0))).toBeTruthy();
     expect(isPositionInWorld(lineWorld, createVector(2.99, 0))).toBeTruthy();
     expect(isPositionInWorld(lineWorld, createVector(0, 0))).toBeTruthy();
 
     // World consisting of a point
-    const pointWorld = createWorld(1, 1);
+    const pointWorld = createWorld(1, 1, 0);
     expect(isPositionInWorld(pointWorld, createVector(0, 0))).toBeTruthy();
     expect(isPositionInWorld(pointWorld, createVector(1, 0))).toBeFalsy();
     expect(isPositionInWorld(pointWorld, createVector(2.99, 0))).toBeFalsy();
 });
 
 test("worldToString test", () => {
-    expect(worldToString(createWorld(3, 2))).toEqual("      \n      ");
-    expect(worldToString(createWorld(1, 5))).toEqual("  \n  \n  \n  \n  ");
+    expect(worldToString(createWorld(3, 2, 0))).toEqual("      \n      ");
+    expect(worldToString(createWorld(1, 5, 0))).toEqual("  \n  \n  \n  \n  ");
 });
 
 test("worldStringVectorToIndex test", () => {
-    expect(worldStringVectorToIndex(createWorld(1, 2), createVector(2, 3))).toThrowError();
-    expect(worldStringVectorToIndex(createWorld(2, 2), createVector(1, 1))).toBe(7);
+    expect(worldStringVectorToIndex(createWorld(1, 2, 0), createVector(2, 3))).toThrowError();
+    expect(worldStringVectorToIndex(createWorld(2, 2, 0), createVector(1, 1))).toBe(7);
 });
