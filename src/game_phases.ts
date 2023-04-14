f12df89d47078ec06102def3e7c6b8d2b5c91c50
DESCOMPS Théo <tdescomps@bordeaux-inp.fr>
2023-04-14 15:10:48 +0200

    healPhase merge

--- src/game_phases.ts
+++ src/game_phases.ts
@@ -11,15 +11,18 @@ function temperatureRisePhase(oldActors: Array<Actor>, phaseResult: Array<Action
 	return oldActors.map((a) => a.kind !== "boss" ? a : { ...a, faith_point: a.faithPoints! - sum(phaseResult) });
 }
 
-function healPhase(oldActors: Array<Actor>, phaseResult: Array<ActionReturnTypes["heal"]>): Array<Actor> {
-	return oldActors.map((a, i) => phaseResult.reduce((actor, curResult) => {
-		const idx = curResult.actorIndices.findIndex((id) => id === i);
-		if (idx !== -1) {
-			const fp = actor.faithPoints ?? 0;
-			return { ...actor, faith_point: fp + curResult.amount[idx] };
-		}
-		return actor;
-	}, a));
+function updateFaithPoints(actor: Actor, actorIndex: number, healResults: Array<ActionReturnTypes["heal"]>): Actor {
+	return {
+		...actor,
+		faithPoints: healResults.reduce((faithPointsAcc, healResult) =>
+					(faithPointsAcc ?? 0) + ((healResult.amount[healResult.actorIndices.indexOf(actorIndex)] ?? 0)),
+					actor.faithPoints)
+	};
+}
+
+function healPhase(oldActors: Array<Actor>, healVectors: Array<ActionReturnTypes["heal"]>): Array<Actor> {
+
+	return oldActors.map((currentActor, actorIndex) => updateFaithPoints(currentActor, actorIndex, healVectors));
 }
 
 function convertEnemiesPhase(oldActors: Array<Actor>, phaseResult: Array<ActionReturnTypes["convertEnemies"]>): Array<Actor> {
