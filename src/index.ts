72ac2c6b7cf3f7edd6bc9096749c7cd4be8801db
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-04-14 14:40:15 +0200

    documented index

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,20 @@ const sprites = [
     document.getElementById("groundSprite"),
 ].map((element) => element as HTMLImageElement);
 
+/**
+ * Returns a promise that will resolve after `ms` seconds.
+ * @param ms delay in ms
+ * @returns a promise that waits `ms` seconds
+ */
 function delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
+/**
+ * Gets the sprite corresponding to an actor's kind
+ * @param actorKind The actor kind
+ * @returns The sprite corresponding to the actor's kind.
+ */
 function getActorSprite(actorKind: Kind): HTMLImageElement {
     switch(actorKind){
         case "ignorant":
@@ -31,6 +41,11 @@ function getActorSprite(actorKind: Kind): HTMLImageElement {
     throw Error("Eslint friendly error, cannot see that all enum values are mapped :)");
 }
 
+/**
+ * Draws the content of the world to the canvas
+ * @param world The world
+ * @param actors The actors
+ */
 async function displayWorldToCanvas(world: World, actors: Array<Actor>){
     // Update canvas
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -45,6 +60,7 @@ async function displayWorldToCanvas(world: World, actors: Array<Actor>){
     // wait
     await delay(1000);
 }
+
 async function main(){
     const world: World = initWorld(7, 7);
 	let actors: Array<Actor> = initActors(world);
