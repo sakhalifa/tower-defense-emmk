43a1dd51b5d41c5593891d396d485c9aa4ad95a1
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2023-04-28 16:14:12 +0200

    Added first test for actor actions

--- tst/actor_actions.test.ts
+++ tst/actor_actions.test.ts
@@ -2,12 +2,20 @@ import { createIgnorant, createSpaghettimonster } from "../src/actor";
 import { temperatureRise } from "../src/actor_actions";
 import { createVector } from "../src/geometry";
 import { createWorld } from "../src/world";
+import { setAttackPower } from "../src/props";
 
 test("TemperatureRise test", () => {
     const world = createWorld(5, 5, 0);
 
     const monster = createSpaghettimonster(createVector(2, 2), 1);
-    const ignorant = createIgnorant(createVector(0, 0), )
+    const ignorant = createIgnorant(createVector(0, 0), createVector(0, 0), undefined);
+    const onPoint = setAttackPower(createIgnorant(createVector(0, 0), createVector(0, 0)), 3);
     
-    expect()
+    const actors = [monster, ignorant, onPoint];
+
+    // The ignorant is not at the same position as the spaghetti monster, so it souldn't attack
+    expect(temperatureRise(actors, ignorant)).toBe(0);
+
+    // Ignorant on the same position as the spaghetti monster, should attack
+    expect(temperatureRise(actors, onPoint)).toBe(3);
 });
