6ca8759c42dfe71edd164b4defbff05e4c94abaa
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2023-05-05 17:04:05 +0200

    Fixed test

--- tst/actor_actions.test.ts
+++ tst/actor_actions.test.ts
@@ -14,16 +14,16 @@ test("TemperatureRise test", () => {
     const actors = [monster, ignorant, onPoint];
 
     // The ignorant is not at the same position as the spaghetti monster, so it souldn't eat the spaghetti monster
-    expect(temperatureRise(actors, ignorant)).toBe(0);
+    expect(temperatureRise(actors, ignorant, world, 'y')).toBe(0);
 
     // Ignorant on the same position as the spaghetti monsteexpect(Stack.stackCreateEmpty()).toEqual({});r, should eat the spaghetti monster
-    expect(temperatureRise(actors, onPoint)).toBe(3);
+    expect(temperatureRise(actors, onPoint, world, 'y')).toBe(3);
 });
 
 
 test("spreadIgnorance test", () => {
-    
-    const ignorant = createIgnorant(createVector(0, 0), createVector(0, 0), undefined);
+    const world = createWorld(5, 5, 0);
+    // const ignorant = createIgnorant(createVector(0, 0), createVector(0, 0), undefined);
     const ignoranceSpreader = setSpreadIgnorancePower(createIgnoranceSpreader(createVector(0, 0), createVector(0, 0)), 3);
     // const actors = [ignorant, ignoranceSpreader];
 
@@ -31,7 +31,7 @@ test("spreadIgnorance test", () => {
     // Spreading faithPoints to a fully ignorant ignorant, should not increase its faithPoints
     // expect(spreadIgnorance(actors, ignoranceSpreader).amount[0]).toBe(0);
     // IgnorantSpreader shouldn't increase its own faithPoints
-    expect(spreadIgnorance([ignoranceSpreader], ignoranceSpreader).amount).toHaveLength(0);
+    expect(spreadIgnorance([ignoranceSpreader], ignoranceSpreader, world, 'y').amount).toHaveLength(0);
     
     // ignorant shouldn't spread faithPoints
     // expect(spreadIgnorance([ignorant], ignorant).amount.length).toBe(0);
